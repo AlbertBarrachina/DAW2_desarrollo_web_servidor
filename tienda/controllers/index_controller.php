8c943011c0d79f56bf60d77c18d79e4be1382e4c
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2023-10-20 20:12:16 +0200

    tienda 10/10/2023

---
+++ tienda/controllers/index_controller.php
@@ -0,0 +1,13 @@
+<?php
+class personas_controller
+{
+    private $con;
+
+    public function __construct()
+    {
+        $this->con = Conectar::conexion();
+    }
+
+    //guarda el alumno creado si no puede da error
+    
+}
