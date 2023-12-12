06114cfd617594fe451c93c646daba989b581970
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2023-12-12 19:43:20 +0100

    seeder de pokemons

---
+++ primerCRUD/seeders/PokemonSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; //libreria nos permite hacer insert
+use Illuminate\Support\Str; //liberaria para funciones str
+
+class PokemonSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //inserta 10 pokemons
+        $pokemons = [
+            'Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise',
+            'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot',
+            'Rattata', 'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash',
+            'Nidoran♀', 'Nidorina', 'Nidoqueen', 'Nidoran♂', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales',
+            'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat',
+            'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe',
+            'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp',
+            'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta', 'Rapidash',
+            'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', 'Farfetch\'d', 'Doduo', 'Dodrio', 'Seel', 'Dewgong', 'Grimer',
+            'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby',
+            'Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak', 'Hitmonlee', 'Hitmonchan', 'Lickitung',
+            'Koffing', 'Weezing', 'Rhyhorn', 'Rhydon', 'Chansey', 'Tangela', 'Kangaskhan', 'Horsea', 'Seadra', 'Goldeen',
+            'Seaking', 'Staryu', 'Starmie', 'Mr. Mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir', 'Tauros',
+            'Magikarp', 'Gyarados', 'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon', 'Omanyte',
+            'Omastar', 'Kabuto', 'Kabutops', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 'Dratini', 'Dragonair',
+            'Dragonite', 'Mewtwo', 'Mew'];
+        $tipos = ['Acero', 'Agua', 'Dragón', 'Eléctrico', 'Fantasma', 'Fuego', 'Hada', 'Hielo', 'Lucha', 'Normal', 'Planta', 'Psíquico', 'Roca', 'Siniestro', 'Tierra', 'Veneno', 'Volador'];
+        $tamanos = ['Pequeño', 'Mediano', 'Grande'];
+        $min = 0;
+        $max = 99999;
+        for ($i = 0; $i <= 9; $i++) {
+            $randomTipo = $tipos[array_rand($tipos)];
+            $randomTamano = $tamanos[array_rand($tamanos)];
+            $randomPokemon = $pokemons[array_rand($pokemons)];
+            if ($randomTamano == 'Pequeño'){
+                $min = 0;
+                $max = 15000;
+            }else if($randomTamano == 'Mediano'){
+                $min = 15100;
+                $max = 45000;
+            }else if($randomTamano == 'Grande'){
+                $min = 45100;
+                $max = 99999;
+            }
+            DB::table('pokemon')->insert([
+                'id' => '0',
+                'nombre' => $randomPokemon,
+                'tipo' => $randomTipo,
+                'tamano' => $randomTamano,
+                'peso' => rand($min, $max) / 100,
+            ]);
+        }
+    }
+}
