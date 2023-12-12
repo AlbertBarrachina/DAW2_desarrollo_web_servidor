708dde72d8c5c716d6b1079e334d7c4f9800760e
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2023-12-12 20:14:41 +0100

    seeder pokemon v1.1

--- primerCRUD/seeders/PokemonSeeder.php
+++ primerCRUD/seeders/PokemonSeeder.php
@@ -14,7 +14,7 @@ class PokemonSeeder extends Seeder
      */
     public function run(): void
     {
-        //inserta 10 pokemons
+        //lista de pokemons para generar
         $pokemons = [
             'Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise',
             'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot',
@@ -31,28 +31,35 @@ class PokemonSeeder extends Seeder
             'Seaking', 'Staryu', 'Starmie', 'Mr. Mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', 'Pinsir', 'Tauros',
             'Magikarp', 'Gyarados', 'Lapras', 'Ditto', 'Eevee', 'Vaporeon', 'Jolteon', 'Flareon', 'Porygon', 'Omanyte',
             'Omastar', 'Kabuto', 'Kabutops', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 'Dratini', 'Dragonair',
-            'Dragonite', 'Mewtwo', 'Mew'];
+            'Dragonite', 'Mewtwo', 'Mew'
+        ];
+        //lista de tipos de pokemons
         $tipos = ['Acero', 'Agua', 'Dragón', 'Eléctrico', 'Fantasma', 'Fuego', 'Hada', 'Hielo', 'Lucha', 'Normal', 'Planta', 'Psíquico', 'Roca', 'Siniestro', 'Tierra', 'Veneno', 'Volador'];
+        //lista de tamaños de poñemons
         $tamanos = ['Pequeño', 'Mediano', 'Grande'];
+        //peso minimo y maximo
         $min = 0;
         $max = 99999;
+        //inserta 10 pokemons
         for ($i = 0; $i <= 9; $i++) {
+            //selecciona el tipo tamaño y nombre aleatoriamente de las arrays anteriores
             $randomTipo = $tipos[array_rand($tipos)];
             $randomTamano = $tamanos[array_rand($tamanos)];
-            $randomPokemon = $pokemons[array_rand($pokemons)];
-            if ($randomTamano == 'Pequeño'){
+            //cambia el rango de peso segun su tamaño
+            if ($randomTamano == 'Pequeño') {
                 $min = 0;
                 $max = 15000;
-            }else if($randomTamano == 'Mediano'){
+            } else if ($randomTamano == 'Mediano') {
                 $min = 15100;
                 $max = 45000;
-            }else if($randomTamano == 'Grande'){
+            } else if ($randomTamano == 'Grande') {
                 $min = 45100;
                 $max = 99999;
             }
+            //insert en la tabla
             DB::table('pokemon')->insert([
                 'id' => '0',
-                'nombre' => $randomPokemon,
+                'nombre' => $pokemons[array_rand($pokemons)],
                 'tipo' => $randomTipo,
                 'tamano' => $randomTamano,
                 'peso' => rand($min, $max) / 100,
